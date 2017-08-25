4efaff18ec9eb4aae76fbd737ce9e8b524a7fbd8
Jarvist <Jarvist@126.com>
2017-08-26 02:49:34 +0800

    3

--- app/src/main/java/com/example/jarvist/remember/MainActivity.java
+++ app/src/main/java/com/example/jarvist/remember/MainActivity.java
@@ -5,10 +5,14 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.support.v7.widget.Toolbar;
+import android.text.Layout;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import org.litepal.crud.DataSupport;
@@ -20,17 +24,23 @@ public class MainActivity extends AppCompatActivity {
     public static final int NAME = 0;//MainActivity
     private RecyclerView recyclerView;
     private List<Note> noteList;
+    private List<Note> deleteList;
     private NoteAdapter adapter;
     private ActionBar actionBar;
+    public int mul_Tag;
+    private CheckBox cb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        ImageButton deletebtn = (ImageButton)findViewById(R.id.menu1);
         TextView title = (TextView)findViewById(R.id.title);
         FloatingActionButton fab = (FloatingActionButton)findViewById(R.id.fab);
         recyclerView = (RecyclerView)findViewById(R.id.recyclerView);
+        cb = (CheckBox)findViewById(R.id.checkbox);
+        CardView cardView = (CardView)findViewById(R.id.cardview);
         setSupportActionBar(toolbar);
         actionBar = getSupportActionBar();
         if(actionBar != null){
@@ -48,11 +58,23 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        deletebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                for(int i=0; i<noteList.size();i++)
+                {
+
+                }
+
+            }
+        });
+
         adapter = new NoteAdapter(noteList);
 
         StaggeredGridLayoutManager manager = new StaggeredGridLayoutManager(2,StaggeredGridLayoutManager.VERTICAL);
         recyclerView.setLayoutManager(manager);
         recyclerView.setAdapter(adapter);
+
 
     }
 
@@ -64,6 +86,20 @@ public class MainActivity extends AppCompatActivity {
         noteList.addAll(newList);
         adapter.notifyDataSetChanged();
     }
+
+    /*@Override
+    public void onBackPressed(){
+        Intent intent = getIntent();
+        mul_Tag = intent.getIntExtra("MUL_TAG",1);
+        if(mul_Tag == NoteAdapter.MUL_TAG)
+        {
+            cb.setVisibility(View.INVISIBLE);
+            mul_Tag = 0;
+        }
+        else
+        super.onBackPressed();
+
+    }*/
 
 
 }
--- app/src/main/java/com/example/jarvist/remember/NoteAdapter.java
+++ app/src/main/java/com/example/jarvist/remember/NoteAdapter.java
@@ -2,8 +2,10 @@ package com.example.jarvist.remember;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.design.widget.Snackbar;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
+import android.text.Layout;
 import android.text.SpannableString;
 import android.text.Spanned;
 import android.text.style.TextAppearanceSpan;
@@ -15,6 +17,7 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
@@ -25,9 +28,10 @@ import java.util.List;
  * Created by Jarvist on 2017/8/24.
  */
 
-public class NoteAdapter extends RecyclerView.Adapter <NoteAdapter.ViewHolder> {
+public class NoteAdapter extends RecyclerView.Adapter <NoteAdapter.ViewHolder>  {
 
     private List<Note> mNoteList;
+
     public static final int NAME = 1;
     private Context mContext;
     //multiple choice
@@ -35,7 +39,16 @@ public class NoteAdapter extends RecyclerView.Adapter <NoteAdapter.ViewHolder> {
     //checkbox situation map
     private HashMap<Integer,Boolean> ischecked;
 
+    public static final int MUL_TAG = 1;
 
+
+
+    public void initMaps(){
+        for(int i = 0; i <mNoteList.size(); i++){
+            ischecked = new HashMap<Integer, Boolean>();
+            ischecked.put(i,false);
+        }
+    }
 
     static class ViewHolder extends RecyclerView.ViewHolder{
         CardView cardView;
@@ -48,12 +61,14 @@ public class NoteAdapter extends RecyclerView.Adapter <NoteAdapter.ViewHolder> {
             cardView = (CardView)v.findViewById(R.id.cardview);
             contentView = (TextView)v.findViewById(R.id.content);
             dateView = (TextView)v.findViewById(R.id.date);
+            checkBox = (CheckBox)v.findViewById(R.id.checkbox);
 
         }
     }
 
     public NoteAdapter (List<Note> noteList){
         mNoteList = noteList;
+        initMaps();
     }
 
 
@@ -64,7 +79,7 @@ public class NoteAdapter extends RecyclerView.Adapter <NoteAdapter.ViewHolder> {
         if(mContext == null){
             mContext = parent.getContext();
         }
-        View view = LayoutInflater.from(mContext).inflate(R.layout.note_item,parent,false);
+        final View view = LayoutInflater.from(mContext).inflate(R.layout.note_item,parent,false);
         final ViewHolder viewHolder = new ViewHolder(view);
 
         viewHolder.cardView.setOnClickListener(new View.OnClickListener() {
@@ -79,6 +94,33 @@ public class NoteAdapter extends RecyclerView.Adapter <NoteAdapter.ViewHolder> {
 
             }
         });
+
+        viewHolder.cardView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                int position = viewHolder.getAdapterPosition();
+                ischecked.put(position,true);
+                viewHolder.checkBox.setChecked(ischecked.get(position));
+                /*Intent intent = new Intent(mContext,MainActivity.class);
+                intent.putExtra("MUL_TAG",MUL_TAG);
+                mContext.startActivity(intent);*/
+                MUL_tag = true;
+                NoteAdapter.super.notifyDataSetChanged();
+                return true;
+            }
+        });
+
+
+
+        viewHolder.checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                int position = viewHolder.getAdapterPosition();
+                Note note = mNoteList.get(position);
+                note.setChecked(isChecked);
+                note.update(note.getId());
+            }
+        });
         return viewHolder;
     }
 
@@ -88,13 +130,19 @@ public class NoteAdapter extends RecyclerView.Adapter <NoteAdapter.ViewHolder> {
         String values = note.getContent();
         holder.contentView.setText(values);
         holder.dateView.setText(new SimpleDateFormat("yyyy/MM/dd    HH:mm:ss").format(note.getDate()));
-
+        if(MUL_tag) {
+            holder.checkBox.setVisibility(View.VISIBLE);
+        }
+        else {
+            holder.checkBox.setVisibility(View.INVISIBLE);
+        }
     }
 
     @Override
     public int getItemCount(){
         return mNoteList.size();
     }
+
 
 
 }
